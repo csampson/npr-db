cb77964305d6263b3ae34c06aa29171c8a5c10ec
Cameron Sampson <csampson.js@gmail.com>
2025-03-03 20:52:44 -0700

    Style fixes

--- scripts/dump-stations.js
+++ scripts/dump-stations.js
@@ -2,13 +2,13 @@
  * @overview Dump stations data from JSON -> Redis commands file
  */
 
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const stationsFile = fs.readFileSync(path.join(__dirname, '../data/stations.json'));
-const stations = JSON.parse(stationsFile.toString('utf8'));
+const stationsFile = fs.readFileSync(path.join(__dirname, '../data/stations.json'))
+const stations = JSON.parse(stationsFile.toString('utf8'))
 
 const attrs = {
   band: '$.band AS band TEXT',
@@ -18,54 +18,54 @@ const attrs = {
   id: '$.id AS id TEXT',
   market_city: '$.market_city AS market_city TEXT',
   market_state: '$.market_state AS market_state TEXT',
-  name: '$.name AS name TEXT',
-};
+  name: '$.name AS name TEXT'
+}
 
-const schema = Object.values(attrs).join(' ');
+const schema = Object.values(attrs).join(' ')
 
-function marshalSetCommand(station) {
+function marshalSetCommand (station) {
   const transformed = Object.keys(attrs).reduce((prev, curr) => {
-    let value = curr === 'geolocation'
+    const value = curr === 'geolocation'
       ? station[curr]?.join(',') || null
-      : station[curr];
+      : station[curr]
 
     return {
       ...prev,
-      [curr]: value,
+      [curr]: value
     }
-  }, {});
+  }, {})
 
-  const stationJSON = JSON.stringify(transformed);
+  const stationJSON = JSON.stringify(transformed)
 
   return `
     JSON.SET ${station.id} $ '${stationJSON.replace("'", "\\'")}'
-  `.trim();
+  `.trim()
 }
 
-function marshalCommands() {
+function marshalCommands () {
   return [
     `FT.CREATE stations ON JSON SCHEMA ${schema}`
   ].concat(
     stations.map(marshalSetCommand)
-  ).join('\n');
+  ).join('\n')
 }
 
-function writeCommands(commands) {
-  fs.writeFileSync(path.join(__dirname, '../data/stations.redis'), commands);
+function writeCommands (commands) {
+  fs.writeFileSync(path.join(__dirname, '../data/stations.redis'), commands)
 }
 
-function execute() {
-  console.info(':: Marshaling Redis commands...');
-  const commands = marshalCommands();
+function execute () {
+  console.info(':: Marshaling Redis commands...')
+  const commands = marshalCommands()
 
-  console.info(':: Writing to `../data/stations.redis`...');
-  writeCommands(commands);
+  console.info(':: Writing to `../data/stations.redis`...')
+  writeCommands(commands)
 
-  console.info(':: Done.');
+  console.info(':: Done.')
 }
 
 try {
-  execute();
-} catch(error) {
-  console.error('!! Failed to dump stations: ', error);
+  execute()
+} catch (error) {
+  console.error('!! Failed to dump stations: ', error)
 }
